2eeccbe65cc4d29301770adc4351c26c385beadd
Diego <diegocaceres1997garcia@gmail.com>
2020-11-19 11:18:50 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+Swal.fire({
+    title: '¡Hola!',
+    text: 'Nuestros amigos estan ansiosos por conocerte.',
+    imageUrl: '/img/perrito.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
